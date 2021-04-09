8e0d8dbc205cff528141462079efbb2a62956bcd
Vincent Zhu <vzhu@cpp.edu>
2021-04-08 23:02:24 -0700

    Initial commit for Kafka consumer demo

---
+++ src/main/java/com/vincent/kafka/springbootkafkaconsumerdemo/SpringBootKafkaConsumerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.kafka.springbootkafkaconsumerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaConsumerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaConsumerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincent/kafka/springbootkafkaconsumerdemo/SpringBootKafkaConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.kafka.springbootkafkaconsumerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaConsumerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
